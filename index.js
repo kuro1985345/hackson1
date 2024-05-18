d519f44ca93c26c1035fd4bd7f1a3281fb69a1e6
uedasatosi <sato1030ryokutya@gmail.com>
2024-05-18 20:42:18 +0900

    #29 Uiの微調整

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//onXlickAdd関数を定義
 const onClickAdd = () =>{
     const textEl = document.getElementById("add-text");
     const text = textEl.value;
     textEl.value= "";
+    //HTMLにタグを作って受け取ったテキストを表示
     const li = document.createElement("li");
     const div = document.createElement("div");
     const p = document.createElement("p");
@@ -9,17 +11,17 @@ const onClickAdd = () =>{
 
     const button = document.createElement("button");
     button.textContent = "完了";
-
+//完了ボタンが押されたときの処理を行う
     button.addEventListener("click",() =>{
         const deleteTarget = button.closest("li");
         document.getElementById("task-list").removeChild(deleteTarget);
     });
-
+//タグの処理
     div.appendChild(p);
     div.appendChild(button);
 
     li.appendChild(div);
     document.getElementById("task-list").appendChild(li);
 };
-
+//ボタンが押された時の処理を記述
 document.getElementById("add-button").addEventListener("click",() => onClickAdd());
