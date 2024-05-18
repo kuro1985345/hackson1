d42fd3e7813f5385c3974b8301fec9ac10a7abc5
uedasatosi <sato1030ryokutya@gmail.com>
2024-05-18 16:50:27 +0900

    [modify] #29  to do　作成処理の実装

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const onClickadd () =>{
-    const textEl = document.getElementById("");
+const onClickadd = () =>{
+    const textEl = document.getElementById("add-text");
     const text = textEl.value;
     textEl= "";
     const li = document.createElement("li");
     const div = document.createElement("div");
     const p = document.createElement("p");
     p.textContent = text;
+
+    const button = document.createElement("button");
+    button.textContent = "完了";
+
+    button.addEventListener("click",() =>{
+        const deleteTarget = button.closest("li");
+        document.getElementById("task-list").removeChild(deleteTarget);
+    });
+
+    div.appendChild(p);
+    div.appendChild(button);
+
+    li.appendChild(div);
+    document.getElementById("task-list").appendChild(li);
 };
+
+document.getElementById("add-button").addEventListener("click",() => onClickadd());
